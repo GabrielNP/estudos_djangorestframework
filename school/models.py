2af538a550fdfd17699f7283dbe805b74753795e
Gabriel Pessighello <gabriel.pessighello@indico.net.br>
2020-12-15 22:32:25 -0300

    Added Course and Student models

--- school/models.py
+++ school/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    LEVEL = (
+        ('B', 'Baisc'),
+        ('I', 'Intermediate'),
+        ('A', 'Advanced'),
+    )
+    course_code = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    level = models.CharField(max_length=1, choices=LEVEL, blank=False, null=False, default='B')
+
+    def __str__(self):
+        return self.description
+
+class Student(models.Model):
+    cour = models.CharField(max_length=30)
+    regional_document = models.CharField(max_length=9)
+    registry_identification = models.CharField(max_length=11)
+    born_at = models.DateField()
+
+    def __str__(self):
+        return self.name
